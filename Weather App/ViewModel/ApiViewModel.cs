c847d4feab5248945ad740ba77fa07ddf119a828
Nyerges Dominik <dommi7797@gmail.com>
2021-05-13 17:59:30 +0200

    added WeatherData classes and api get request + result parse.

---
+++ Weather App/ViewModel/ApiViewModel.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using Weather_App.Model;
+
+namespace Weather_App.ViewModel
+{
+    class ApiViewModel
+    {
+        private const string _APIKEY = "http://api.openweathermap.org/data/2.5/forecast?q=Budapest&units=metric&appid=b03caa60c8b499890c552d1690aaccb6";
+
+        public WeatherData.Root GetWeatherForcast()
+        {
+            using(var webClient = new WebClient())
+            {
+                var rawJson = webClient.DownloadString(_APIKEY);
+                var result = JsonConvert.DeserializeObject<WeatherData.Root>(rawJson);
+                return result;
+            }
+        }
+    }
+}
